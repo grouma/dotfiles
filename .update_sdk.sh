2f8a09f4fcd7d1cb49aa73dae4a8c27a378da335
Gary Roumanis <groumanis@gmail.com>
2019-01-18 20:22:51 +0000

    update dart

---
+++ .update_sdk.sh
@@ -0,0 +1,19 @@
+rm -rf ~/usr/lib/dart
+
+if [ "$(uname)" == "Darwin" ]; then
+	PLATFORM="macos-ia32"
+else
+	PLATFORM="linux-x64"
+fi
+
+if [ -z "$1" ]; then
+	VERSION="latest"
+else
+	VERSION="$1"
+fi
+
+echo "Downloading latest dart"
+DOWNLOAD="https://storage.googleapis.com/dart-archive/channels/dev/release/"
+VERSION_PATH="$VERSION/sdk/dartsdk-$PLATFORM-release.zip"
+curl $DOWNLOAD$VERSION_PATH -o /tmp/dart-release.zip
+unzip -qo /tmp/dart-release.zip -d ~
